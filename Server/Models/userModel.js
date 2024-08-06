dc0e57e6c9d9f54fe7d5f3d8726e93b41ce98418
Michael Amos <mikimiki2194@gmail.com>
2024-08-06 11:08:05 +0100

    Created server environment with userModel and userRoutes

---
+++ Server/Models/userModel.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bycryptjs';
+
+const userSchema = new mongoose.Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId, required: true, auto: true },
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: [true, 'Please enter email'], lowercase: true, trim: true },
+    role: { type: String, required: true, default: 'user' },
+    address: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
+    createdAt: { type: Date, default: Date.now, immutable: true, required: true },
+    password: { type: String, required: [true, 'Please enter password'], minlength: [8, 'password must be at least 8 character'], select: false},
+})
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
---
+++ Server/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './config.env' });
+
+// import userRoutes from './Routes/userRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT || 7800;
+
+let MONGO_URI = '';
+if (process.env.NODE_ENV === 'production') {
+  MONGO_URI = process.env.HOSTED_CONN;
+} else {
+  MONGO_URI = process.env.LOCAL_CONN;
+}
+
+app.use(cors());
+app.use(express.json());
+
+// app.use('/api/v1/user', userRoutes);
+
+app.use('/', (req, res) => {
+  res.send('This is the default response');
+});
+
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
