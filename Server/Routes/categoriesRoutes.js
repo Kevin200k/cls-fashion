08d96f43721974f054d4db4809b026756a49a997
Michael Amos <mikimiki2194@gmail.com>
2024-08-11 15:10:07 +0100

    Enabled deleting off categories

--- Server/Routes/categoriesRoutes.js
+++ Server/Routes/categoriesRoutes.js
@@ -41,4 +41,24 @@ router.get('/categories', async(req, res, next) => {
     }
 })
 
+// router.delete('/categories/:categoriesId', protect, requireVerification, Restrict('admin'), async(req, res, next) => {
+router.delete('/categories/:categoriesId', async(req, res, next) => {
+    const { categoriesId } = req.body;
+
+    try{
+        const category = await Categories.find({_id: categoriesId});
+        if(!category){
+            res.status(401).json({ message: "The category does not exist" })
+        }
+
+        await Categories.findOneAndDelete({_id: categoriesId})
+        logEvent(`Categories ${categoriesId} has been deleted`);
+        res.status(201).json({message: "Category successfully deleted"})
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+
 export default router
